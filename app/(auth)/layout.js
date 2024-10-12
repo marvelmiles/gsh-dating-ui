412c3c2d8aee37cea13f02b084dd66b4a0edda30
marvelmiles <marvellousabidemi2@gmail.com>
2024-10-12 02:01:43 +0100

    mod origin

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -2,7 +2,7 @@ import AppRootLayout from "@/components/AppRootLayout";
 
 export const metadata = {
   title: "Soulmater Authentication",
-  description: "Soulmater is a platform that connect you with your soulmate",
+  description: "Soulmater is a platform that connects you with your soulmate.",
 };
 
 export default function RootLayout({ children }) {
